2f5b25608fdd640e957a1fc8aa30a4f5ba7f1a55
Enchan <enchant0130@gmail.com>
2020-07-17 23:09:23 +0900

    [Modify] gitignore

---
+++ SwiitaTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiitaTestApp
+//
+//  Created by EnchantCode on 2020/07/17.
+//  Copyright © 2020 EnchantCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
