8a94185094dad273b4565e3654220f0f60146801
Enchan <enchant0130@gmail.com>
2020-07-17 22:02:39 +0900

    Initial Commit

---
+++ Swiita/Swiita.h
@@ -0,0 +1,19 @@
+//
+//  Swiita.h
+//  Swiita
+//
+//  Created by EnchantCode on 2020/07/17.
+//  Copyright © 2020 EnchantCode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swiita.
+FOUNDATION_EXPORT double SwiitaVersionNumber;
+
+//! Project version string for Swiita.
+FOUNDATION_EXPORT const unsigned char SwiitaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiita/PublicHeader.h>
+
+
