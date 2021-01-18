8a56ba283aa3ab075ce5c28be9e3a2f66cbdfdca
Enchan <enchant0130@gmail.com>
2021-01-18 12:41:09 +0900

    [Modify] test code

--- SwiitaTests/SwiitaTests.swift
+++ SwiitaTests/SwiitaTests.swift
@@ -14,10 +14,7 @@ class SwiitaTests: XCTestCase {
     // 実行引数
     let environment = ProcessInfo.processInfo.environment
 
-    // APIアクセス情報
-    var clientID: String?
-    var clientSecret: String?
-    var token: String?
+    var swiita: Swiita?
 
     // 初期設定
     override func setUpWithError() throws {
@@ -36,56 +33,43 @@ class SwiitaTests: XCTestCase {
             fatalError("Environmental variables had been passed, but it's empty")
         }
 
-        self.clientID = clientID
-        self.clientSecret = clientSecret
-        self.token = token
+        // Swiitaインスタンスを生成
+        print("Swiita instance prepared.")
+        self.swiita = Swiita(clientid: clientID!, clientsecret: clientSecret!, apihost: nil, token: token!)
     }
 
     // テスト後の処理
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-
     }
 
-    func testExample() {
-        print("Hello, World!!")
-    }
+    /* -- APIエンドポイントの動作テスト -- */
 
-    // QiitaAPIの各エンドポイントをテストする
-    func oldtestQiitaAPIs() throws {
-        // Swiitaインスタンス初期化に必要な情報が渡されているか?
-        let clientID = self.environment["QIITA_CLIENT_ID"] ?? ""
-        let clientSecret = self.environment["QIITA_CLIENT_SECRET"] ?? ""
-        let token = self.environment["QIITA_ACCESS_TOKEN"] ?? ""
-        print("##########################################")
-        print("\(clientID)\n\(clientSecret)\n\(token)\n")
-        print("##########################################")
-
-        if clientID == "" || clientSecret == "" || token == "" {
-            XCTFail("[SwiitaRunnerTest] Invalid parameter")
-        }
-
-        let swiita = Swiita(clientid: clientID, clientsecret: clientSecret, token: token)
-
-        //  APIリクエスト
+    /// 認証済みユーザオブジェクト取得
+    func testGetAuthenticatedUser() throws {
         let exp = expectation(description: "Get authenticated user")
 
-        getAuthenticatedUser(swiita: swiita, exp: exp)
-
-        wait(for: [exp], timeout: 10.0)
-    }
-
-    // 認証済ユーザ取得
-    func getAuthenticatedUser(swiita: Swiita, exp: XCTestExpectation) {
-        swiita.getAuthenticatedUser(success: { (status, _) in
+        self.swiita?.getAuthenticatedUser(success: { (status, json) in
+            // HTTPステータスコードを確認
             XCTAssert(status == .Successful, "[SwiitaTests] http status error: \(status)")
 
-            exp.fulfill()
-        }) { (error) in
-            XCTFail("[SwiitaTests] getAuthenticatedUser: \(error)")
+            // ユーザ名が取得できれば表示
+            guard
+                let userName = json["name"].string,
+                let userID = json["id"].string
+            else {
+                XCTFail("Invalid json")
+                return
+            }
+            print("Logged in with: \(userName) (\(userID))")
 
             exp.fulfill()
-        }
+        }, failure: { (error) in
+            XCTFail(error.localizedDescription)
+            exp.fulfill()
+        })
+
+        wait(for: [exp], timeout: 5)
     }
 
 }
